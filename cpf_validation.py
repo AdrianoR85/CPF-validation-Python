375dfffc008da2d48159dfac56301e19382c8d46
AdrianoR85 <adrianorosa085@gmail.com>
2023-09-27 16:18:04 -0300

    Added cpf_generator function

--- cpf_validation.py
+++ cpf_validation.py
@@ -1,7 +1,11 @@
-print('Fomart valid: xxx.xxx.xxx-xx')
-input_cpf = input('Enter with a CPF: ')
-cpf_without_dot = ''.join(input_cpf[0:11].split('.'))
-counter = 10
+import random
+import sys
+
+def cpf_generator():
+  nine_digits = ''
+  for i in range(9):
+    nine_digits += str(random.randint(0,9))
+  return nine_digits
 
 def sum_of_digits(cpf, counter):
   sum = 0
@@ -10,39 +14,65 @@ def sum_of_digits(cpf, counter):
     counter -=1
   return sum
 
-
 def get_one_digit(result_sum_digits):
-  new_digit = ''
   rest = (result_sum_digits * 10) % 11
-  new_digit = str(0) if rest > 9 else str(rest)
-    
-  return new_digit
+  return  str(0) if rest > 9 else str(rest)
    
+     
+def add_digit(cpf, counter):
+  result_of_sum = sum_of_digits(cpf, counter)
+  digit = get_one_digit(result_of_sum)
+  return new_cpf + digit
 
-def format_cpf(cpf):
-  new_cpf = ''
-  for i in range(0, len(cpf)):
-    if i == 3 or i == 6:
-      new_cpf += '.'
-    
-    if i == 9:
-      new_cpf += '-'
-  
-    new_cpf += cpf[i]  
-  return new_cpf
-  
-  
-result_of_sum = sum_of_digits(cpf_without_dot, counter)
-first_digit = get_one_digit(result_of_sum)
-cpf_without_dot += first_digit
 
+def verification(cpf1, cpf2):
+  if cpf1 == (cpf2[0] * 11):
+    return False
+  elif cpf1 != cpf2:
+    return False
+  else: 
+    return True
+
+def show_message(option, validation):
+  if option == 1:
+    print(f'CPF: {new_cpf}')
+  
+  if option == 2:
+    if validation:
+      print('CPF is valid')
+    else: 
+      print('CPF is not valid')
+
+# =================================================================
+
+print('Choose a of options')
+print('1 - CPF Generate')
+print('2 - CPF Validate')
+
+input_cpf = ''
+option = ''
+
+while True:
+  option = input('Option: ')
+  option = int(option)
+  if option == 1:
+    input_cpf = cpf_generator()
+    break
+  elif option == 2:
+    input_cpf = input_cpf = input('Enter with a CPF: ') \
+      .replace('.', '') \
+      .replace('-', '')
+    break
+  else:
+    print('Invalid option')
+    continue
+  
+new_cpf = input_cpf[:9]     
+counter = 10
+
+new_cpf = add_digit(new_cpf, counter)
 counter += 1
+new_cpf = add_digit(new_cpf, counter)
 
-result_of_sum = sum_of_digits(cpf_without_dot, counter)
-second_digit = get_one_digit(result_of_sum)
-cpf_without_dot += second_digit    
-
-new_cpf = format_cpf(cpf_without_dot)
-
-message = 'CPF is valid' if input_cpf == new_cpf else 'CPF is not valid'
-print(message)
+validation = verification(input_cpf, new_cpf)
+show_message(option, validation) 
